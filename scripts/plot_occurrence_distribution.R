edbb55ea25a27be4b3b8508feb9ac379a68ff300
= <=>
2021-04-10 21:54:39 +0200

    Changed additional plots to work with data actually used in the analysis

--- scripts/plot_occurrence_distribution.R
+++ scripts/plot_occurrence_distribution.R
@@ -27,22 +27,23 @@ col.group = c("Coleoptera" = "chartreuse",
 in_region <- fread("data/indices_in_region.csv")
 
 #load data
-dat.occ.full <- fread(
-  here("data", "occurrences_full_refined.csv")) %>%
-  
-  # exclude non - georeferenced and records outside germany
-  # this has to happen at the end, else the number of rows does not match
-  drop_na(decimalLatitude) %>% 
-  filter(in_region) %>% 
-  
-  #remove records without determined species
-  filter(species != "") %>%
-  
-  #exclude first and last days
-  filter(doy != 1, doy != 366, doy != 365) %>% 
-  
-  #only include records from year.start on
-  filter(year >= year.start & year <= year.stop)
+# dat.occ.full <- fread(
+#   here("data", "occurrences_full_refined.csv")) %>%
+#   
+#   # exclude non - georeferenced and records outside germany
+#   # this has to happen at the end, else the number of rows does not match
+#   drop_na(decimalLatitude) %>% 
+#   filter(in_region) %>% 
+#   
+#   #remove records without determined species
+#   filter(species != "") %>%
+#   
+#   #exclude first and last days
+#   filter(doy != 1, doy != 366, doy != 365) %>% 
+#   
+#   #only include records from year.start on
+#   filter(year >= year.start & year <= year.stop)
+dat.occ.full <- fread("data/occurrences_full_pruned.csv")
 
 
 # Generate plots ----------------------------------------------------------
