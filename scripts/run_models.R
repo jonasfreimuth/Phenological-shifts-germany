d930b22117b0836dce59d06d224022d6f7f6f4df
jonas.freimuth <68993915+jonasfreimuth@users.noreply.github.com>
2021-06-27 00:02:46 +0200

    Improved raw data modeling script
    * Dropping of rows without temperature records
    * Extraction of random effects with TMP package instead of mixedup
    * Centering of dependent variables
    * Addition of overall model parameter to random effects

--- scripts/run_models.R
+++ scripts/run_models.R
@@ -2,11 +2,11 @@
 # Setup -------------------------------------------------------------------
 
 library("glmmTMB")
-library("mixedup")
 library("stringr")
 library("data.table")
 library("here")
 library("dplyr")
+library("tidyr")
 
 source("scripts/functions.R")
 
@@ -31,6 +31,15 @@ dat.occ <- readRDS("data/DT_after_nonGer_exclusion_SpeciesFilteredAgain.rds") %>
   # reduce dataset, only use relevant columns
   select(matches(select_cols)) %>% 
   
+  # remove records w/o determined temp
+  drop_na(temp) %>% 
+  
+  # center main independent variables to a mean of 0
+  mutate(temp = temp - mean(temp),
+         year = year - mean(year),
+         lat = lat - mean(lat),
+         long = long - mean(long)) %>% 
+  
   # for testing purposes, limit number of observations
   slice_sample(n = 1000)
 
@@ -45,9 +54,15 @@ for (form in form_vec) {
   
   mod_form <- as.formula(form)
   
-  # extract response and fist dependent variable
-  simple_form <- str_split(form, " ", simplify = TRUE)[, 1:3] %>%
+  # split model formula into constituents
+  mod_comps <- str_split(form, " ", simplify = TRUE)
+  
+  # extract response and fist independent variable
+  simple_form <- mod_comps[, 1:3] %>%
     paste(collapse = "")
+  
+  # extract main independent var
+  main_var <- mod_comps[, 3]
   
   log_msg("Starting", simple_form, "model...")
   
@@ -74,7 +89,16 @@ for (form in form_vec) {
   
   log_msg("Extracting random effects for", simple_form, "model...")
   
-  rnd_eff <- extract_random_effects(glm_mod)
+  # extract intercept and main slope
+  # this is not very elegant and might break
+  intercept <- glm_mod$fit$par[1]
+  slope <- glm_mod$fit$par[2]
+  
+  # extract random effects, add them to overall effects to obtain proper 
+  # estimate
+  rnd_eff <- ranef(glm_mod)$cond[[1]]
+  rnd_eff[["(Intercept)"]] <- rnd_eff[["(Intercept)"]] + intercept
+  rnd_eff[[main_var]] <- rnd_eff[[main_var]] + slope
   
   fwrite(rnd_eff, paste0("data/glmm_rnd_eff_",
                          str_replace(simple_form, "~", "_"), "_",
