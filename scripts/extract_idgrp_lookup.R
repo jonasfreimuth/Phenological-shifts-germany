b679670e9895cbafe87c73d94bfccd429c370e93
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2021-09-24 11:58:47 +0200

    Cleanup scripts no longer in use

--- scripts/extract_idgrp_lookup.R
+++
@@ -1,8 +0,0 @@
-# TODO: put this in front of pruning script so that this lookup includes every
-# species potentially in the dataset
-dat.occ <- readRDS("data/DT_after_nonGer_exclusion_SpeciesFilteredAgain.rds")
-
-lookup <- unique(dat.occ %>% select(species, id.grp))
-
-write.csv(lookup, "data/id_grp_species_lookup.csv",
-          row.names = FALSE)
