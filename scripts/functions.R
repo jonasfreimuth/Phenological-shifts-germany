20829fd6c82351221ed4d721b1e2981c7b612408
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2021-11-18 14:21:59 +0100

    add function to extract significance labs from ph
    * uses table extracted from Tukes-HSD output to find groups with
      non-significant pairwise differences and outputs a vector of
      group labels

--- scripts/functions.R
+++ scripts/functions.R
@@ -53,6 +53,98 @@ log_msg <- function(...) {
 }
 
 
+# function for giving a vector of letters reflecting significance of pairwise
+#   differences. Optionally respects factor ordering if provided
+sig_letters <- function(ph_table, ordering = NULL, alpha = 0.05) {
+  
+  # set vector of lables
+  lables <- LETTERS
+  
+  unq_lvls <- unique(as.vector(str_split(row.names(ph_table),
+                                         "(?<=\\w)-(?=\\w)",
+                                         simplify = TRUE)))
+  
+  # if no order is provided, use the order items appear in the group matrix
+  if (is.null(ordering)) {
+    
+    ordering <- unq_lvls
+    
+  } else {
+    
+    if (!length(setdiff(ordering, unq_lvls)) == 0) {
+      stop("Provided order vector does not cover all levels found in data.")
+    }
+    
+  }
+  
+  n <- length(ordering)
+  
+  # get rows with pvals above the threshold, i.e. levels that DO NOT differ
+  sel_vec <- ph_table[ , 4] > alpha
+  
+  # # ENSURE WE GET A MATRIX BC IN CASE OF ONLY ONE ROW RETURNING IT IS A VECTOR
+  # # THIS NEITHER HAPPENS WITH 0 OR <=2 ROWS
+  # non_sig_tab <- as.matrix(ph_table[ph_table[ , 4] > alpha, ])
+  
+  grp_mat <- str_split(rownames(ph_table)[sel_vec],
+                       "(?<=\\w)-(?=\\w)",
+                       simplify = TRUE)
+  
+  pair_mat <- matrix(0, nrow = n, ncol = n, 
+                   dimnames = list(ordering, ordering))
+  
+  # if everything is sig. different, give out corresponding vector
+  if (nrow(grp_mat) == 0) {
+    return(setNames(lables[1:length(ordering)], ordering))
+  }
+  
+  # fill pair mat, 1 will indicate a non sig diff pairing
+  for (lvl in ordering) {
+    
+    lvl_ind <- which(ordering == lvl)
+    
+    for (i in 1:nrow(grp_mat)) {
+      cont_vec <- grp_mat[i, ] == lvl
+      
+      if (any(cont_vec)) {
+        # get index of the partner of current lvl in this non-sig diff
+        prt_ind <- which(ordering == grp_mat[i, !cont_vec])
+        
+        # mark group membership in matrix
+        pair_mat[lvl_ind, prt_ind] <- 1
+      }
+    }
+  }
+  
+  # get list of groups by turning pair matrix into a graph, finding max cliques
+  #  and extracting the vertex names 
+  grp_list <- igraph::graph_from_adjacency_matrix(pair_mat,
+                                     mode = "undirected") %>% 
+    igraph::max_cliques() %>%
+    lapply(names) %>% 
+    lapply(function(x, ordering) {
+      x[order(match(x, ordering))]
+    }, ordering)
+  
+  # order list so that first vector element of each list element follows 
+  #   ordering
+  # get ordering vec for the list according to the order of the first element
+  ord_vec <- order(match(sapply(grp_list, function(x) { x[1] }), ordering))
+  grp_list <- grp_list[ord_vec]
+  
+    
+  out_vec <- setNames(vector("character", n), ordering)
+  
+  for (i in 1:length(grp_list)) {
+    ind_vec <- ordering %in% grp_list[[i]]
+    
+    out_vec[ind_vec] <- paste0(out_vec[ind_vec], lables[i])
+  }
+  
+  return(out_vec)
+}
+
+
 # bioflor trait functions -------------------------------------------------
 
 
