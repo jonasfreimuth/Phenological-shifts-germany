b9b937f6aafb8b35fc3315b7579865145cb18415
jonas.freimuth <68993915+jonasfreimuth@users.noreply.github.com>
2021-12-05 13:58:02 +0100

    add multiple analysis variations

---
+++ scripts/citsci_plants/run_occ_refine.R
@@ -0,0 +1,27 @@
+# read extracted occurence.txt, do stuff with it and save it
+# the actual refinement
+fread(paste(sep = "/", "download", paste0("occurrence_", k, ".txt")),
+      quote = "", showProgress = FALSE,
+      select = data_cols) %>% 
+  
+  
+  # exclude records without days
+  filter(day != "") %>% 
+  
+  # reformat the date to POSIXct and calculate DOY
+  mutate(date = as.Date(substr(eventDate, 1, 10))) %>%
+  select(-eventDate) %>%
+  mutate(doy = as.integer(strftime(date, "%j"))) %>%
+  mutate(decade = floor(year / 10) * 10) %>% 
+  
+  # add group label
+  mutate(id.grp = if_else(kingdom == "Plantae",
+                          "Plants",
+                          order)) %>% 
+  
+  # filter out plants without trait data
+  filter(!(id.grp == "Plants" & !(species %in% bioflor_traits$species))) %>% 
+  
+  # save data as csv
+  fwrite(paste0(data_dir, "occurrences_full.csv"),
+         append = TRUE)
